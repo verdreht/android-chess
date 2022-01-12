5cc0c10311dfe534a7a4c72aefc7bfa90e6a9c28
renznberga <jakob.anzengruber@gmx.at>
2022-01-12 19:00:14 +0100

    test branch1

--- app/src/main/java/net/nightcodes/androidchess/MainActivity.java
+++ app/src/main/java/net/nightcodes/androidchess/MainActivity.java
@@ -36,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
+        //aölkhdgnqewihavdfnl
+
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
