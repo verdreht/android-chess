a5795ec3ff34ed4ff2307665df799e97aad726fa
renznberga <jakob.anzengruber@gmx.at>
2022-01-29 21:51:56 +0100

    Created Branch ONLY FOR SERVERSOCKET EXPERIMENTS

--- app/src/main/java/net/nightcodes/androidchess/server/Server.java
+++ app/src/main/java/net/nightcodes/androidchess/server/Server.java
@@ -1,18 +1,9 @@
 package net.nightcodes.androidchess.server;
 
-import android.util.Log;
-
 import net.nightcodes.androidchess.Host;
 import net.nightcodes.androidchess.utils.Utils;
 
-import org.snf4j.core.SelectorLoop;
-import org.snf4j.core.factory.AbstractSessionFactory;
-import org.snf4j.core.handler.IStreamHandler;
-
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
 
 public class Server {
 
@@ -32,21 +23,7 @@ public class Server {
         host.addLogEntry("[SERVER] Lobby has been created! >> " + serverName);
         host.addLogEntry("[SERVER] Initialized server! >> " + Utils.getIPAddress(true) + ":" + port);
 
-        SelectorLoop loop = new SelectorLoop();
-        loop.start();
-
-        ServerSocketChannel channel = ServerSocketChannel.open();
-        channel.configureBlocking(false);
-        channel.socket().bind(new InetSocketAddress(Utils.getIPAddress(true), port));
-
-        loop.register(channel, new AbstractSessionFactory() {
-            @Override
-            protected IStreamHandler createHandler(SocketChannel channel) {
-                return new ServerHandler(host);
-            }
-        });
-
-        loop.join();
+        
 
     }
 
