d944f3d823effa0cabb9773c655dcb8e130d538f
renznberga <jakob.anzengruber@gmx.at>
2022-02-01 19:57:31 +0100

    added canMove method of Bishop

--- app/src/main/java/net/nightcodes/androidchess/game/entity/Bishop.java
+++ app/src/main/java/net/nightcodes/androidchess/game/entity/Bishop.java
@@ -8,8 +8,9 @@ import net.nightcodes.androidchess.game.entity.base.EntityIdentification;
 import net.nightcodes.androidchess.game.entity.base.IEntity;
 import net.nightcodes.androidchess.game.entity.base.ImageAssetType;
 import net.nightcodes.androidchess.game.logic.MoveResult;
+import net.nightcodes.androidchess.game.logic.board.Board;
 import net.nightcodes.androidchess.game.logic.board.EntityColor;
-import net.nightcodes.androidchess.game.logic.movement.Location;
+import net.nightcodes.androidchess.game.logic.board.Field;
 import net.nightcodes.androidchess.game.logic.movement.MovementPermission;
 
 import java.util.HashMap;
@@ -33,8 +34,25 @@ public class Bishop implements IEntity<Bishop> {
     }
 
     @Override
-    public MoveResult canMove(Location location) {
-        return null;
+    public MoveResult canMove(Field currentLocation, Field nextMoveLocation, Board board) {
+        MoveResult moveResult = MoveResult.NOT_PERMITTED;
+        for (int i = 1; i < 6; i++) {
+            if (((nextMoveLocation.getFieldLocation().getY() == currentLocation.getFieldLocation().getY() + i) ||
+                    (nextMoveLocation.getFieldLocation().getY() == currentLocation.getFieldLocation().getY() - i)) &&
+                    ((nextMoveLocation.getFieldLocation().getX() == currentLocation.getFieldLocation().getX() + i) ||
+                            (nextMoveLocation.getFieldLocation().getX() == currentLocation.getFieldLocation().getX() - i))) {
+                moveResult = MoveResult.PERMITTED;
+                break;
+            } else if (((nextMoveLocation.getFieldLocation().getY() == currentLocation.getFieldLocation().getY() + i) ||
+                    (nextMoveLocation.getFieldLocation().getY() == currentLocation.getFieldLocation().getY() - i)) &&
+                    ((nextMoveLocation.getFieldLocation().getX() == currentLocation.getFieldLocation().getX() + i) ||
+                            (nextMoveLocation.getFieldLocation().getX() == currentLocation.getFieldLocation().getX() - i)) &&
+                    (nextMoveLocation.getFieldEntity().getEntityColor().ordinal() != currentLocation.getFieldEntity().getEntityColor().ordinal())) {
+                moveResult = MoveResult.ENTITY_HIT;
+                break;
+            }
+        }
+        return moveResult;
     }
 
     @Override
